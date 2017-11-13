c262494b898328503a090daa016ae8ce09620f3e
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-13 16:59:35 +0000

    programs are not responding after sending one message

--- GamesProgramming/Ex_Audio/Network.cpp
+++ GamesProgramming/Ex_Audio/Network.cpp
@@ -47,7 +47,7 @@ void Network::runUdpServer(unsigned short port)
 {
 	// Create a socket to receive a message from anyone
 	sf::UdpSocket socket;
-	socket.setBlocking(false);
+	//socket.setBlocking(false);
 
 	// Listen to messages on the specified port
 	if (socket.bind(port) != sf::Socket::Done)
@@ -69,7 +69,6 @@ void Network::runUdpServer(unsigned short port)
 		return;
 	std::cout << "Message sent to the client: \"" << out << "\"" << std::endl;
 }
-
 
 ////////////////////////////////////////////////////////////
 /// Send a message to the server, wait for the answer
@@ -120,7 +119,7 @@ void Network::runUdpClient(unsigned short port)
 	std::size_t received;
 	sf::IpAddress sender;
 	unsigned short senderPort;
-	switch (socket.receive(in, sizeof(in), received, sender, senderPort) != sf::Socket::Done)
+	switch (socket.receive(in, sizeof(in), received, sender, senderPort))
 	{
 		/*case sf::Socket::NotReady :
 		std::cout << "Socket not ready " << server << std::endl;
@@ -145,10 +144,75 @@ void Network::runUdpClient(unsigned short port)
 
 }
 
+////////////////////////////////////////////////////////////
+/// Launch a server, wait for a message, send an answer.
+///
+////////////////////////////////////////////////////////////
+//void Network::runUdpServer(unsigned short port)
+//{
+//	// Create a socket to receive a message from anyone
+//	sf::UdpSocket socket;
+//	//socket.setBlocking(false);
+//
+//	// Listen to messages on the specified port
+//	if (socket.bind(port) != sf::Socket::Done)
+//		return;
+//	std::cout << "Server is listening to port " << port << ", waiting for a message... " << std::endl;
+//
+//	// Wait for a message
+//	char in[128];
+//	std::size_t received;
+//	sf::IpAddress sender;
+//	unsigned short senderPort;
+//	if (socket.receive(in, sizeof(in), received, sender, senderPort) != sf::Socket::Done)
+//		return;
+//	std::cout << "Message received from client " << sender << ": \"" << in << "\"" << std::endl;
+//
+//	// Send an answer to the client
+//	const char out[] = "Hi, I'm the server";
+//	if (socket.send(out, sizeof(out), sender, senderPort) != sf::Socket::Done)
+//		return;
+//	std::cout << "Message sent to the client: \"" << out << "\"" << std::endl;
+//}
+//
+//
+//////////////////////////////////////////////////////////////
+///// Send a message to the server, wait for the answer
+/////
+//////////////////////////////////////////////////////////////
+//void Network::runUdpClient(unsigned short port)
+//{
+//	// Ask for the server address
+//	sf::IpAddress server;
+//	do
+//	{
+//		std::cout << "Type the address or name of the server to connect to: ";
+//		std::cin >> server;
+//	} while (server == sf::IpAddress::None);
+//
+//	// Create a socket for communicating with the server
+//	sf::UdpSocket socket;
+//	socket.setBlocking(false);
+//
+//	// Send a message to the server
+//	const char out[] = "Hi, I'm a client";
+//	if (socket.send(out, sizeof(out), server, port) != sf::Socket::Done)
+//		return;
+//	std::cout << "Message sent to the server: \"" << out << "\"" << std::endl;
+//
+//	// Receive an answer from anyone (but most likely from the server)
+//	char in[128];
+//	std::size_t received;
+//	sf::IpAddress sender;
+//	unsigned short senderPort;
+//	if (socket.receive(in, sizeof(in), received, sender, senderPort) != sf::Socket::Done)
+//		return;
+//	std::cout << "Message received from " << sender << ": \"" << in << "\"" << std::endl;
+//}
+
 void Network::update(float dt)
 {
-	//runUdpServer(port);
-	// Choose an arbitrary port for opening sockets
+	//runUdpClient(port);
 
 	// TCP, UDP or connected UDP ?
 	char protocol;
