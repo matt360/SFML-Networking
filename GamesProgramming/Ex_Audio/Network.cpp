eaa9307ba59bc9f5e025cc5d9eb159d1df39f7a7
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-13 17:19:23 +0000

    removed old code

--- GamesProgramming/Ex_Audio/Network.cpp
+++ GamesProgramming/Ex_Audio/Network.cpp
@@ -77,12 +77,12 @@ void Network::runUdpServer(unsigned short port)
 void Network::runUdpClient(unsigned short port)
 {
 	// Ask for the server address
-	sf::IpAddress server;
-	do
+	sf::IpAddress server("127.1.0");
+	/*do
 	{
-		std::cout << "Type the address or name of the server to connect to: ";
-		std::cin >> server;
-	} while (server == sf::IpAddress::None);
+	std::cout << "Type the address or name of the server to connect to: ";
+	std::cin >> server;
+	} while (server == sf::IpAddress::None);*/
 
 	// Create a socket for communicating with the server
 	sf::UdpSocket socket;
@@ -93,24 +93,24 @@ void Network::runUdpClient(unsigned short port)
 
 	switch (socket.send(out, sizeof(out), server, port))
 	{
-		/*case sf::Socket::NotReady :
+	case sf::Socket::NotReady:
 		std::cout << "Socket not ready " << server << std::endl;
-		break;*/
-
-
-		/*case sf::Socket::Disconnected :
-
-		break;*/
-
-		/*case sf::Socket::Error :
-
-		break;*/
+		break;
 
 	case sf::Socket::Done:
 		std::cout << "Message sent to the server: \"" << out << "\"" << std::endl;
 		break;
 
+	case sf::Socket::Disconnected:
+		std::cout << "Disconnected" << std::endl;
+		break;
+
+	case sf::Socket::Error:
+		std::cout << "Socket Error" << std::endl;
+		break;
+
 	default:
+		std::cout << "Default Error" << std::endl;
 		return;
 	}
 
@@ -121,94 +121,27 @@ void Network::runUdpClient(unsigned short port)
 	unsigned short senderPort;
 	switch (socket.receive(in, sizeof(in), received, sender, senderPort))
 	{
-		/*case sf::Socket::NotReady :
+	case sf::Socket::NotReady:
 		std::cout << "Socket not ready " << server << std::endl;
-		break;*/
-
-
-		/*case sf::Socket::Disconnected :
-
-		break;*/
-
-		/*case sf::Socket::Error :
-
-		break;*/
+		break;
 
 	case sf::Socket::Done:
 		std::cout << "Message received from " << sender << ": \"" << in << "\"" << std::endl;
 		break;
 
+	case sf::Socket::Disconnected:
+		std::cout << "Disconnected" << std::endl;
+		break;
+
+	case sf::Socket::Error:
+		std::cout << "Socket Error" << std::endl;
+		break;
+
 	default:
+		std::cout << "Default Error" << std::endl;
 		return;
 	}
-
 }
-
-////////////////////////////////////////////////////////////
-/// Launch a server, wait for a message, send an answer.
-///
-////////////////////////////////////////////////////////////
-//void Network::runUdpServer(unsigned short port)
-//{
-//	// Create a socket to receive a message from anyone
-//	sf::UdpSocket socket;
-//	//socket.setBlocking(false);
-//
-//	// Listen to messages on the specified port
-//	if (socket.bind(port) != sf::Socket::Done)
-//		return;
-//	std::cout << "Server is listening to port " << port << ", waiting for a message... " << std::endl;
-//
-//	// Wait for a message
-//	char in[128];
-//	std::size_t received;
-//	sf::IpAddress sender;
-//	unsigned short senderPort;
-//	if (socket.receive(in, sizeof(in), received, sender, senderPort) != sf::Socket::Done)
-//		return;
-//	std::cout << "Message received from client " << sender << ": \"" << in << "\"" << std::endl;
-//
-//	// Send an answer to the client
-//	const char out[] = "Hi, I'm the server";
-//	if (socket.send(out, sizeof(out), sender, senderPort) != sf::Socket::Done)
-//		return;
-//	std::cout << "Message sent to the client: \"" << out << "\"" << std::endl;
-//}
-//
-//
-//////////////////////////////////////////////////////////////
-///// Send a message to the server, wait for the answer
-/////
-//////////////////////////////////////////////////////////////
-//void Network::runUdpClient(unsigned short port)
-//{
-//	// Ask for the server address
-//	sf::IpAddress server;
-//	do
-//	{
-//		std::cout << "Type the address or name of the server to connect to: ";
-//		std::cin >> server;
-//	} while (server == sf::IpAddress::None);
-//
-//	// Create a socket for communicating with the server
-//	sf::UdpSocket socket;
-//	socket.setBlocking(false);
-//
-//	// Send a message to the server
-//	const char out[] = "Hi, I'm a client";
-//	if (socket.send(out, sizeof(out), server, port) != sf::Socket::Done)
-//		return;
-//	std::cout << "Message sent to the server: \"" << out << "\"" << std::endl;
-//
-//	// Receive an answer from anyone (but most likely from the server)
-//	char in[128];
-//	std::size_t received;
-//	sf::IpAddress sender;
-//	unsigned short senderPort;
-//	if (socket.receive(in, sizeof(in), received, sender, senderPort) != sf::Socket::Done)
-//		return;
-//	std::cout << "Message received from " << sender << ": \"" << in << "\"" << std::endl;
-//}
 
 void Network::update(float dt)
 {
