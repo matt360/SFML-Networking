093daedc585a07db2080b22ec1388a2cda4441ba
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-21 18:01:43 +0000

    linear interpolation working

--- GamesProgramming/Ex_Audio/GameClient.cpp
+++ GamesProgramming/Ex_Audio/GameClient.cpp
@@ -328,7 +328,7 @@ void GameClient::checkForIncomingPackets()
 			// Deal with the messages from the packet
 			// Put position into history of network positions
 
-			if (network_positions.size() > 3) network_positions.pop();
+			if (network_positions.size() > 1) network_positions.pop();
 				network_positions.push(player_message_receive);
 			
 			/*if (network_positions.size() == 3)
@@ -428,11 +428,7 @@ void GameClient::update()
 	if ((int)fps % 6 == 0)
 		sendPacket();
 
-	// start
 	checkForIncomingPackets();
-	// end
-
-	std::cout << "\n\ncurrent time: " << *offset << "\n\n";
 
 	// TODO keep track of local positions
 
@@ -448,22 +444,22 @@ void GameClient::update()
 	// You need to update:
 	// - the predicted position at the current time, in "x_" and "y_"
 	std::cout << "function call: getCurrentTime(): " << getCurrentTime() << "\n";
-	//float x_average_velocity, y_average_velocity;
-	//PlayerMessage msg0 = network_positions.front(); network_positions.pop();
-	//PlayerMessage msg1 = network_positions.front(); network_positions.pop();
-	//float time = (float)(*offset);
+	float x_average_velocity, y_average_velocity;
+	PlayerMessage msg0 = network_positions.front(); 
+	PlayerMessage msg1 = network_positions.back(); 
+	float time = getCurrentTime();
 
 	//// average velocity = (recieved_position - last_position) / (recieved_time - last_time)
-	//x_average_velocity = (msg0.x - msg1.x) / (msg0.time - msg1.time);
-	//y_average_velocity = (msg0.y - msg1.y) / (msg0.time - msg1.time);
+	x_average_velocity = (msg0.x - msg1.x) / (msg0.time - msg1.time);
+	y_average_velocity = (msg0.y - msg1.y) / (msg0.time - msg1.time);
 
 	//// linear model
-	//float x_, y_;
-	//x_ = x_average_velocity * (time - msg1.time) + msg1.x;
-	//y_ = y_average_velocity * (time - msg1.time) + msg1.y;
+	float x_, y_;
+	x_ = x_average_velocity * (time - msg1.time) + msg1.x;
+	y_ = y_average_velocity * (time - msg1.time) + msg1.y;
 
-	//sf::Vector2f loc_player_pos(x_, y_);
-	//player.setPosition(loc_player_pos);
+	sf::Vector2f loc_player_pos(x_, y_);
+	player.setPosition(loc_player_pos);
 
 	//// quadratic model
 	//float 
