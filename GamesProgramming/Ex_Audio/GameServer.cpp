f852f2674d5d2ecddf280b6ef32e25982c47a017
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-22 14:25:25 +0000

    not working inheritance

--- GamesProgramming/Ex_Audio/GameServer.cpp
+++ GamesProgramming/Ex_Audio/GameServer.cpp
@@ -157,7 +157,7 @@ void GameServer::render()
 
 	endDraw();
 }
-
+//
 void GameServer::beginDraw()
 {
 	window->clear(sf::Color(0, 0, 0));
@@ -194,37 +194,13 @@ bool GameServer::checkSphereBounding(Sprite* s1, Sprite* s2)
 	return false;
 }
 
-void GameServer::displayMessage(const PlayerMessage player_message)
-{
-	// The message from the client
-	std::cout << "\n\nSERVER: Message received from the client:";
-	// Data extracted successfully...
-	std::cout << "\nSERVER: ID: " << player_message.id
-		<< "\nSERVER: Player x: " << player_message.position.x
-		<< "\nSERVER: Player y: " << player_message.position.y
-		<< "\nSERVER: Time: " << player_message.time;
-}
-
-void GameServer::displayMessage(const PlayerMessage player_message, const sf::IpAddress sender, const unsigned short sender_port)
-{
-	// The message from the client
-	std::cout << "\n\nSERVER: Message received from the client:";
-	// Data extracted successfully...
-	std::cout << "\nSERVER: ID: " << player_message.id
-		<< "\nSERVER: Player x: " << player_message.position.x
-		<< "\nSERVER: Player y: " << player_message.position.y
-		<< "\nSERVER: Time: " << player_message.time;
-	std::cout << "\nSERVER: client's IP: " << sender;
-	std::cout << "\nSERVER: client's port: " << sender_port;
-}
-
 void GameServer::addMessage(PlayerMessage& player_message_send)
 {
 	//PlayerMessage player_message_send;
 	player_message_send.id = 0;
 	player_message_send.position.x = player.getPosition().x;
 	player_message_send.position.y = player.getPosition().y;
-	
+
 	player_message_send.time = clock->getElapsedTime().asMilliseconds();
 }
 
--- GamesProgramming/Ex_Audio/GameState.h
+++ GamesProgramming/Ex_Audio/GameState.h
@@ -1,7 +1,16 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <SFML/Network.hpp>
+#include <Windows.h>
+#include <iostream>
+#include <string.h>
+#include <mutex>
 #include "Input.h"
+#include "Player.h"
+#include "AnimatedSprite.h"
+#include "Map.h"
+#include "AudioManager.h"
+#include "Messages.h"
 #include "State.h"
 
 class GameState : public State
@@ -9,4 +18,31 @@ class GameState : public State
 public:
 	GameState();
 	~GameState();
+
+	//virtual void render();
+
+protected:
+	float fps;
+
+	sf::Text error_text;
+
+	AudioManager audioMgr;
+
+	bool hasStarted;
+
+	// Game Variables
+	Player player;
+	//Enemy enemy;
+	//Cursor cursor;
+	sf::Texture texture;
+
+	Map level;
+
+	// sound test
+	sf::SoundBuffer buff;
+	sf::Sound soun;
+
+	virtual void addMessage(PlayerMessage& player_message) = 0;
+	void displayMessage(const PlayerMessage player_message);
+	void displayMessage(const PlayerMessage player_message, const sf::IpAddress ip_address, const unsigned short port);
 };
