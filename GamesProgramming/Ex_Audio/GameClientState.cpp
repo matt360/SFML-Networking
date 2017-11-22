36b57140ed04bf87b6c5492bf2a62eb3957448d8
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-22 20:10:08 +0000

    removed collision detection from GameClientState

--- GamesProgramming/Ex_Audio/GameClientState.cpp
+++ GamesProgramming/Ex_Audio/GameClientState.cpp
@@ -168,32 +168,6 @@ void GameClientState::handleInput()
 	}
 }
 
-// check AABB
-bool GameClientState::checkCollision(Sprite* s1, Sprite* s2)
-{
-	if (s1->getAABB().left + s1->getAABB().width < s2->getAABB().left)
-		return false;
-	if (s1->getAABB().left > s2->getAABB().left + s2->getAABB().width)
-		return false;
-	if (s1->getAABB().top + s1->getAABB().height < s2->getAABB().top)
-		return false;
-	if (s1->getAABB().top > s2->getAABB().top + s2->getAABB().height)
-		return false;
-
-	return true;
-}
-
-// check Sphere bounding collision
-bool GameClientState::checkSphereBounding(Sprite* s1, Sprite* s2)
-{
-	// half width, height, give us the centre of the shape
-	if (pow(s2->getPosition().x - s1->getPosition().x, 2) + pow(s2->getPosition().y - s1->getPosition().y, 2) < pow((s2->getSize().x / 2) + (s1->getSize().x / 2), 2))
-	{
-		return true;
-	}
-	return false;
-}
-
 void GameClientState::addMessage(PlayerMessage& player_message_send)
 {
 	//PlayerMessage player_message_send;
