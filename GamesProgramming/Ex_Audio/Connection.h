1b6b5ac98c3faea83bbebb48c941132d9a84da85
Matthew Wallace <1502616@abertay.ac.uk>
2017-11-28 19:59:35 +0000

    connection dev

--- GamesProgramming/Ex_Audio/Connection.h
+++ GamesProgramming/Ex_Audio/Connection.h
@@ -1,19 +1,33 @@
 /* The Connection class */
-
 #pragma once
-
+#include <SFML/Network.hpp>
+#include <SFML/Main.hpp>
+#include "Network.h"
 #include <WinSock2.h>
 
 // Each instance of this class represents a connected client.
-class Connection
+class Connection : public Network
 {
 public:
 	// Constructor.
 	// sock: the socket that we've accepted the client connection on.
 	Connection(SOCKET sock);
+	Connection();
 
 	// Destructor.
 	~Connection();
+
+	/// socket
+	//static sf::UdpSocket socket;
+	/// Choose an arbitrary port for opening sockets
+	//unsigned short port;
+	/// IP adrress to connect to
+	//sf::IpAddress ip_address;
+	/// connection handler - does not let the server start without establishing the connection with the client
+	//bool established_connection;
+	
+	/// get cthe current time and take the offset into account
+	//sf::Int32 getCurrentTime(const sf::Clock& clock, const sf::Int32& offset);
 
 	// Process an incoming message.
 	void processMessage(const NetMessage *message);
@@ -52,5 +66,3 @@ private:
 	char writeBuffer_[100 * sizeof NetMessage];
 	int writeCount_;
 };
-
-#endif
